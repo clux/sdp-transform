176c9b0006170a9ce0edeaa1a44d04bfca50937e
clux <analsandblaster@gmail.com>
2013-07-21 14:45:08 +0100

    rewrite so that regexp and naming logic is next to each other, and parser is separate

--- parser.js
+++ parser.js
@@ -1,191 +1,161 @@
-var r = {
-  line : /^([a-z])=(.*)/,
-  //c=IN IP4 10.47.197.26
-  cIp: /^IN IP(\d) (.*)/,
-  //b=AS:4000
-  bLine: /(TIAS|AS|CT)\:(\d*)/,
-  //m=video 51744 RTP/AVP 126 97 98 34 31
-  mRtp: /(\w*) (\d{4,5}) RTP\/(S?)AVP(F?) (.*)/,
-  //a=rtpmap:110 MP4A-LATM/90000
-  aRtp: /^rtpmap\:(\d*) (\w*)\/(\d*)/,
-  //a=fmtp:108 profile-level-id=24;object=23;bitrate=64000
-  aFmtp: /^fmtp\:(\d*) (.*)/,
-  //a=setup:actpass
-  aSetup: /^setup\:(\w*)/,
-  //a=mid:1
-  aMid: /^mid\:(\d*)/,
-  //a=sendrecv
-  aSendRecv: /^(sendrecv|recvonly|sendonly|inactive)/,
-  //a=fingerprint:SHA-1 00:11:22:33:44:55:66:77:88:99:AA:BB:CC:DD:EE:FF:00:11:22:33
-  aFinger: /^fingerprint\:(\S*) (.*)/,
-  //a=ice-ufrag:F7gI
-  aIceUfrag: /^ice\-ufrag\:(.*)/,
-  //a=ice-pwd:x9cml/YzichV2+XlhiMu8g
-  aIcePwd: /^ice\-pwd\:(.*)/,
-  //a=candidate:0 1 UDP 2113667327 203.0.113.1 54400 typ host
-  aCandidate: /^candidate:(\S*) (\d*) (\S*) (\d*) (\S*) (\d*) typ (\S*)/
+var lineReg = /^([a-z])=(.*)/;
+var regs = {
+  c: [
+    { //c=IN IP4 10.47.197.26
+      name: 'connection',
+      reg: /^IN IP(\d) (.*)/,
+      names: ['version', 'ip']
+    }
+  ],
+  b: [
+    { //b=AS:4000
+      push: 'bandwidth',
+      reg: /(TIAS|AS|CT|RR|RS)\:(\d*)/,
+      names: ['type', 'limit']
+    }
+  ],
+  m: [
+    { //m=video 51744 RTP/AVP 126 97 98 34 31
+      //NB: special - can write directly to `location` does not need a 'name'
+      // TODO: rtp/fmtp should filter by the payloads found here
+      reg: /(\w*) (\d{4,5}) ([A-Z\/]*) (.*)?/,
+      names: ['type', 'port', 'protocol', 'payloads']
+    }
+  ],
+  a: [
+    // TODO: add a separator somehow to indicate mLine only properties
+    { //a=rtpmap:110 MP4A-LATM/90000
+      push: 'rtp',
+      reg: /^rtpmap\:(\d*) (\w*)\/(\d*)/,
+      names: ['payload', 'codec', 'rate']
+    },
+    { //a=fmtp:108 profile-level-id=24;object=23;bitrate=64000
+      push: 'fmtp',
+      reg: /^fmtp\:(\d*) (.*)/,
+      names: ['payload', 'config']
+    },
+    { //a=setup:actpass
+      name: 'setup',
+      reg: /^setup\:(\w*)/
+    },
+    { //a=mid:1
+      name: 'mid',
+      reg: /^mid\:(\d*)/,
+    },
+    { //a=ptime:20
+      name: 'ptime',
+      reg: /^ptime\:(\d*)/
+    },
+    { //a=sendrecv
+      name: 'sendrecv',
+      reg: /^(sendrecv|recvonly|sendonly|inactive)/,
+    },
+    { //a=fingerprint:SHA-1 00:11:22:33:44:55:66:77:88:99:AA:BB:CC:DD:EE:FF:00:11:22:33
+      name: 'fingerprint',
+      reg: /^fingerprint\:(\S*) (.*)/,
+      names: ['type', 'hash']
+    },
+    { //a=ice-ufrag:F7gI
+      name: 'iceUfrag',
+      reg: /^ice\-ufrag\:(.*)/,
+    },
+    { //a=ice-pwd:x9cml/YzichV2+XlhiMu8g
+      name: 'icePwd',
+      reg: /^ice\-pwd\:(.*)/,
+    },
+    // TODO: verify these are m-line only
+    { //a=candidate:0 1 UDP 2113667327 203.0.113.1 54400 typ host
+      push: 'candidates',
+      reg: /^candidate:(\S*) (\d*) (\S*) (\d*) (\S*) (\d*) typ (\S*)/,
+      names: ['foundation', 'component', 'transport', 'priority', 'ip', 'port', 'type']
+    }
+  ]
 };
 
-// fmtpConfigs are parsed to an object where all values are left as strings
-// unless they clearly are integers
+var toIntIfInt = function (v) {
+  return String(Number(v)) === v ? Number(v) : v;
+};
 var fmtpReducer = function (acc, expr) {
   var s = expr.split('=');
   if (s.length === 2) {
-    acc[s[0]] = String(Number(s[1])) === s[1] ? Number(s[1]) : s[1];
+    acc[s[0]] = toIntIfInt(s[1]);
   }
   return acc;
+};
+
+var parseReg = function (match, location, names, rawName) {
+  if (rawName && !names) {
+    location[rawName] = toIntIfInt(match[1]);
+  }
+  else {
+    for (var i = 0; i < names.length; i += 1) {
+      location[names[i]] = toIntIfInt(match[i+1]);
+    }
+  }
 };
 
 var parse = function (sdp) {
   var lines = sdp.split('\n');
 
-  // keep track of which mLine we are in
+  var session = {}; // session data not related to an m-line
   var mLines = [];
-  var mLineIdx = -1; // by default not inside any mLine
-
-  var meta = {ice: {}}; // meta data not related to an m-line
+  session.media = mLines; // link it up
+  var location = session;  // will be updated to point at current mLine
 
   for (var i = 0; i < lines.length; i += 1) {
     var l = lines[i];
-    if (!r.line.test(l)) {
+    if (!lineReg.test(l)) {
       continue; // skip lines we dont understand
     }
-    var match = l.match(r.line);
+    var match = l.match(lineReg);
+    var type = match[1];
+    var content = match[2];
+    if (!regs[type]) {
+      continue; // skip lines without matching grammar
+    }
+    if (type === 'm') {
+      mLines.push({rtp: [], fmtp: []});
+      location = mLines[mLines.length-1];
+    }
 
-    var content = match[2]; // everything to the left of the equals
-    switch (match[1]) {
-      // skip these header ones for now
-    case "v": // protocol version
-    case "s":
-      break;
-    case "t":
-      break;
-    case "i": // session information || media information
-      var savePt = (mLineIdx < 0) ? meta : mLines[mLineIdx];
-      savePt.information = content;
-      break;
-    case "o": // session identifier
-      meta.identifier = content;
-      break;
-    case "b":
-      if (r.bLine.test(content)) {
-        var bMatch = content.match(r.bLine);
-        var savePt = (mLineIdx < 0) ? meta : mLines[mLineIdx];
-        savePt.bandwidth = {
-          type: bMatch[1],
-          bandwidth: bMatch[2] | 0
-        };
-      }
-      break; // bandwidth TODO!
-
-    case "m":  // actual media
-      if (r.mRtp.test(content)) {
-        mLineIdx += 1;
-        var mMatch = content.match(r.mRtp);
-        mLines.push({
-          type: mMatch[1], // audio/video/application/..
-          port: mMatch[2] | 0,
-          encrypted: !!mMatch[3], // true => required
-          feedback: !!mMatch[4], // may be false but still use rtcp feedback..
-          maps: mMatch[5].split(' ').map(Number),
-          rtpMaps: [],
-          fmtpMaps: [],
-          ice: {
-            candidates: []
-          }
-        });
-      }
-      break;
-    case "a":
-      // (potentially) global properties
-      if (r.aIcePwd.test(content)) {
-        var aMatch = content.match(r.aIcePwd);
-        var savePt = (mLineIdx < 0) ? meta : mLines[mLineIdx];
-        savePt.ice.pwd = aMatch[1];
-      }
-      else if (r.aIceUfrag.test(content)) {
-        var aMatch = content.match(r.aIceUfrag);
-        var savePt = (mLineIdx < 0) ? meta : mLines[mLineIdx];
-        savePt.ice.ufrag = aMatch[1];
-      }
-
-      if (mLineIdx < 0) {
-        continue;
-      }
-
-      if (r.aCandidate.test(content)) { // TODO: verify these are m-line only
-        var aMatch = content.match(r.aCandidate);
-        mLines[mLineIdx].ice.candidates.push({
-          foundation: aMatch[1],
-          component: aMatch[2] | 0,
-          transport: aMatch[3],
-          priority: aMatch[4] | 0,
-          ip: aMatch[5],
-          port: aMatch[6] | 0,
-          type: aMatch[7]
-        });
-      }
-
-      // properties affecting the current m-line
-      if (r.aRtp.test(content)) {
-        var aMatch = content.match(r.aRtp);
-        // add the rtpmap if it was advertised in the main m-line
-        if (mLines[mLineIdx].maps.indexOf(aMatch[1] | 0) >= 0) {
-          mLines[mLineIdx].rtpMaps.push({
-            payload: aMatch[1] | 0,
-            codec: aMatch[2],
-            rate: aMatch[3] | 0
-          });
+    for (var j = 0; j < regs[type].length; j += 1) {
+      var obj = regs[type][j];
+      var needsObject = obj.name && obj.names;
+      if (obj.reg.test(content)) {
+        if (obj.push && !location[obj.push]) {
+          location[obj.push] = [];
         }
-      }
-      else if (r.aFmtp.test(content)) {
-        var aMatch = content.match(r.aFmtp);
-        if (mLines[mLineIdx].maps.indexOf(aMatch[1] | 0) >= 0) {
-          mLines[mLineIdx].fmtpMaps.push({
-            payload: aMatch[1] | 0,
-            config: aMatch[2].split(';').reduce(fmtpReducer, {})
-          });
+        else if (needsObject && !location[obj.name]) {
+          location[obj.name] = {};
         }
-      }
-      else if (r.aMid.test(content)) {
-        var aMatch = content.match(r.aMid);
-        mLines[mLineIdx].mid = aMatch[1] | 0;
-      }
-      else if (r.aSetup.test(content)) {
-        var aMatch = content.match(r.aSetup);
-        mLines[mLineIdx].setup = aMatch[1];
-      }
-      else if (r.aFinger.test(content)) {
-        var aMatch = content.match(r.aFinger);
-        mLines[mLineIdx].encryption = {
-          type: aMatch[1],
-          fingerprint: aMatch[2]
-        };
-      }
-      else if (r.aSendRecv.test(content)) {
-        var aMatch = content.match(r.aSendRecv);
-        mLines[mLineIdx].sendrecv = aMatch[1];
-      }
-      break;
+        var keyLocation = obj.push ? 
+          {} :  // blank object that will be pushed
+          needsObject ? location[obj.name] : location; // otherwise, named location or root
 
-    case "c": // connection info (could be global or under an m-line)
-      if (r.cIp.test(content)) {
-        var ipMatch = content.match(r.cIp);
-        // NB: ipMatch[1] | 0 is IP version
-        var savePt = (mLineIdx < 0) ? meta : mLines[mLineIdx];
-        savePt.connection = ipMatch[2];
+        parseReg(content.match(obj.reg), keyLocation, obj.names, obj.name);
+        if (obj.push) {
+          location[obj.push].push(keyLocation);
+        }
+        break;
       }
-      break;
-    default:
-      // fine as long as kept in sync with r.line
-      throw new Error("unimplemented line type");
+    }
+    continue;
+  }
+
+  // post processing
+  for (var i = 0; i < mLines.length; i += 1) {
+    var l = mLines[i];
+    if (l.payloads) {
+      l.payloads = l.payloads.split(' ').map(Number);
+    }
+    for (var j = 0; j < l.fmtp.length; j += 1) {
+      if (l.fmtp[j].config) {
+        l.fmtp[j].config = l.fmtp[j].config.split(';').reduce(fmtpReducer, {});
+      }
     }
   }
 
-  return {
-    meta: meta,
-    media: mLines
-  };
+  return session;
 };
 
 module.exports = parse;
@@ -194,6 +164,5 @@ if (module === require.main) {
   var fs = require('fs');
   var sdp = fs.readFileSync('./test/normal.sdp')+'';
   var res = parse(sdp);
-  console.log(res.meta);
-  console.log(res.media);
+  console.log(res.media[1]);
 }
--- test/basic.js
+++ test/basic.js
@@ -10,59 +10,58 @@ test("normal.sdp", function (t) {
       t.end();
       return;
     }
-    var res = parse(sdp+'');
-    var meta = res.meta;
-    var media = res.media;
-    t.ok(meta, "got meta info");
+    var session = parse(sdp+'');
+    t.ok(session, "got session info");
+    var media = session.media;
     t.ok(media && media.length > 0, "got media");
 
-    t.equal(meta.identifier, '- 20518 0 IN IP4 203.0.113.1', 'identifier');
-    t.equal(meta.connection, '203.0.113.1');
+    //t.equal(session.identifier, '- 20518 0 IN IP4 203.0.113.1', 'identifier');
+    t.equal(session.connection.ip, '203.0.113.1');
 
     // global ICE and fingerprint
-    t.equal(meta.ice.ufrag, "F7gI", "global ufrag");
-    t.equal(meta.ice.pwd, "x9cml/YzichV2+XlhiMu8g", "global pwd");
+    t.equal(session.iceUfrag, "F7gI", "global ufrag");
+    t.equal(session.icePwd, "x9cml/YzichV2+XlhiMu8g", "global pwd");
 
     var audio = media[0];
     t.equal(audio.type, "audio", "audio");
     t.equal(audio.port, 54400, "audio port");
-    t.ok(audio.encrypted, "audio encrypted");
+    t.equal(audio.protocol, "RTP/SAVPF", "audio protocol");
     t.equal(audio.sendrecv, "sendrecv", "audio sendrecv");
-    t.equal(audio.rtpMaps[0].payload, 0, "audio rtp 0 payload");
-    t.equal(audio.rtpMaps[0].codec, "PCMU", "audio rtp 0 codec");
-    t.equal(audio.rtpMaps[0].rate, 8000, "audio rtp 0 rate");
-    t.equal(audio.rtpMaps[1].payload, 96, "audio rtp 1 payload");
-    t.equal(audio.rtpMaps[1].codec, "opus", "audio rtp 1 codec");
-    t.equal(audio.rtpMaps[1].rate, 48000, "audio rtp 1 rate");
+    t.equal(audio.rtp[0].payload, 0, "audio rtp 0 payload");
+    t.equal(audio.rtp[0].codec, "PCMU", "audio rtp 0 codec");
+    t.equal(audio.rtp[0].rate, 8000, "audio rtp 0 rate");
+    t.equal(audio.rtp[1].payload, 96, "audio rtp 1 payload");
+    t.equal(audio.rtp[1].codec, "opus", "audio rtp 1 codec");
+    t.equal(audio.rtp[1].rate, 48000, "audio rtp 1 rate");
 
     var video = media[1];
     t.equal(video.type, "video", "video");
     t.equal(video.port, 55400, "video port");
-    t.ok(video.encrypted, "video encrypted");
+    t.equal(video.protocol, "RTP/SAVPF", "video protocol");
     t.equal(video.sendrecv, "sendrecv", "video sendrecv");
-    t.equal(video.rtpMaps[0].payload, 97, "video rtp 0 payload");
-    t.equal(video.rtpMaps[0].codec, "H264", "video rtp 0 codec");
-    t.equal(video.rtpMaps[0].rate, 90000, "video rtp 0 rate");
-    t.equal(video.fmtpMaps[0].payload, 97, "video fmtp 0 payload");
-    t.equal(video.fmtpMaps[0].config['profile-level-id'], "4d0028", "video fmtp 0 profile-level-id");
-    t.equal(video.fmtpMaps[0].config['packetization-mode'], 1, "video fmtp 0 packetization-mode");
-    t.equal(video.rtpMaps[1].payload, 98, "video rtp 1 payload");
-    t.equal(video.rtpMaps[1].codec, "VP8", "video rtp 1 codec");
-    t.equal(video.rtpMaps[1].rate, 90000, "video rtp 1 rate");
+    t.equal(video.rtp[0].payload, 97, "video rtp 0 payload");
+    t.equal(video.rtp[0].codec, "H264", "video rtp 0 codec");
+    t.equal(video.rtp[0].rate, 90000, "video rtp 0 rate");
+    t.equal(video.fmtp[0].payload, 97, "video fmtp 0 payload");
+    t.equal(video.fmtp[0].config['profile-level-id'], "4d0028", "video fmtp 0 profile-level-id");
+    t.equal(video.fmtp[0].config['packetization-mode'], 1, "video fmtp 0 packetization-mode");
+    t.equal(video.rtp[1].payload, 98, "video rtp 1 payload");
+    t.equal(video.rtp[1].codec, "VP8", "video rtp 1 codec");
+    t.equal(video.rtp[1].rate, 90000, "video rtp 1 rate");
 
-        // ICE candidates (same for both audio and video in this case)
-    [audio.ice.candidates, video.ice.candidates].forEach(function (cs, i) {
+    // ICE candidates (same for both audio and video in this case)
+    [audio.candidates, video.candidates].forEach(function (cs, i) {
       var str = (i === 0) ? "audio " : "video ";
       var port = (i === 0) ? 54400 : 55400;
       t.equal(cs.length, 2, str + "got 2 candidates");
-      t.equal(cs[0].foundation, "0", str + "ice candidate 0 foundation");
+      t.equal(cs[0].foundation, 0, str + "ice candidate 0 foundation");
       t.equal(cs[0].component, 1, str + "ice candidate 0 component");
       t.equal(cs[0].transport, "UDP", str + "ice candidate 0 transport");
       t.equal(cs[0].priority, 2113667327, str + "ice candidate 0 priority");
       t.equal(cs[0].ip, "203.0.113.1", str + "ice candidate 0 ip");
       t.equal(cs[0].port, port, str + "ice candidate 0 port");
       t.equal(cs[0].type, "host", str + "ice candidate 0 type");
-      t.equal(cs[1].foundation, "1", str + "ice candidate 1 foundation");
+      t.equal(cs[1].foundation, 1, str + "ice candidate 1 foundation");
       t.equal(cs[1].component, 2, str + "ice candidate 1 component");
       t.equal(cs[1].transport, "UDP", str + "ice candidate 1 transport");
       t.equal(cs[1].priority, 2113667326, str + "ice candidate 1 priority");
