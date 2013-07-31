9b41bb063f128d50b7f345a501336c43f69a815e
Lance Stout <lancestout@gmail.com>
2013-07-30 17:52:05 -0700

    Add remote-candidates attribute support and parser.

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ exports.write = writer;
 exports.parse = parser.parse;
 exports.parseFmtpConfig = parser.parseFmtpConfig;
 exports.parsePayloads = parser.parsePayloads;
+exports.parseRemoteCandidates = parser.parseRemoteCandidates;
